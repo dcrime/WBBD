9e9f1adf42e89f00555c651b9621d541f96747f4
dcrime <ppp.dddd7@gmail.com>
2021-07-12 01:20:54 +0200

    Automated the notifications
    The notifications are automated now, they still need animations
    Some error handling and a random token generator

--- Code/bot.js
+++ Code/bot.js
@@ -1,15 +1,21 @@
 // @ts-check
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const randexp = require('randexp');
 const bot = new Discord.Client()
 /** @param {string} token */
 let botLogin = async token => new Promise(async resolve => {
-    let reg = RegExp('^[a-zA-Z0-9-_]{24}\.[a-zA-Z0-9-_]{6}\.[a-zA-Z0-9-_]{27}');
-    
-    token.match(reg) || resolve(false);
-    (await bot.login(token)).length || resolve(false);
+    let reg = RegExp('^[a-zA-Z0-9\\-_]{24}\\.[a-zA-Z0-9\\-_]{6}\\.[a-zA-Z0-9\\-_]{27}');
+    /**
+     * 0 - Invalid Token Format, Random Token
+     * 1 - Invalid Token
+     * 2 - Successful Login
+     */
+    console.log(new randexp(reg).gen())
+    if(!token.match(reg)) return resolve({ err: true, info: 0, randToken: new randexp(reg).gen() });
+    bot.login(token).catch(e => {resolve({ err: true, info: e.code == 'TOKEN_INVALID' ? 1 : 4, e })})
 
     bot.on('ready', () => {
         console.log(`Logged in as ${bot.user.tag}`)
-        resolve(true)
+        resolve({err: false, info: 2})
     });
 })
--- Code/main.js
+++ Code/main.js
@@ -1,4 +1,4 @@
-// const { client } = require("./global");
+// const { client, createElement } = require("./global");
 
 // @ts-check
 globalThis.createElement = (tag, options) => {
@@ -55,6 +55,8 @@ function login(token = null) {
     let tokenValue = document.getElementById('loginToken')
     let loginButton = document.getElementById('loginButton')
     let errorBox = document.getElementById('errorBox')
+    let i = 0;
+
 
     if (token == null) {
         // @ts-ignore
@@ -62,18 +64,57 @@ function login(token = null) {
     } else {
         // @ts-ignore
         botLogin(tokenValue.value).then(confirm => {
-            if (confirm) {
-                start(), document.getElementById('loginScreen').style.display = 'none'
-            } else {
-                loginButton.animate([
-                    { backgroundColor: '#333333' },
-                    { backgroundColor: '#330000' },
-                    { backgroundColor: '#333333' }
-                ], { duration: 300, iterations: 2 });
-                console.log('Incorrect token')
+            switch (confirm.info) {
+                case 2:
+                    start(), document.getElementById('loginScreen').style.display = 'none'
+                    showNotif('Login Successful', [
+                        { type: 'infoDefault', content: 'You have logged into the bot' },
+                    ])
+                    break;
+                case 1:
+                    showNotif('Invalid Token', [
+                        { type: 'infoDefault', content: 'The token you entered is invalid' },
+                        { type: 'infoDefault', content: 'Please check if you misspelled the token' },
+                    ])
+                    break;
+                case 0:
+                    showNotif('Invalid Token format', [
+                        { type: 'infoDefault', content: 'The formatting of the token you entered is invalid' },
+                        { type: 'infoDescBold', content: 'EXAMPLE:' },
+                        { type: 'infoSmall', content: confirm.randToken }
+                    ])
+                    break;
+
+                default:
+                    showNotif('Unknown Error', [
+                        { type: 'infoDefault', content: confirm.e },
+                    ])
+                    break;
             }
+
+            loginButton.animate([
+                { backgroundColor: '#333333' },
+                { backgroundColor: '#330000' },
+                { backgroundColor: '#333333' }
+            ], { duration: 300, iterations: 1 })
         })
     }
+}
+
+function showNotif(title, contents) {
+    let notificationPlaceholder = document.getElementById('notificationPlaceholder')
+    let infoBox = createElement('div', { id: 'infoBox' })
+    infoBox.appendChild(createElement('div', { id: 'infoSVG' }))
+    let infoTitle = createElement('div', { id: 'infoTitle' })
+    infoTitle.innerText = title
+    infoBox.appendChild(infoTitle)
+    for (let elem of contents) {
+        let desc = createElement('div', { id: elem.type })
+        desc.innerText = elem.content
+        infoBox.appendChild(desc)
+    }
+    notificationPlaceholder.insertBefore(infoBox, notificationPlaceholder.firstChild);
+    setTimeout(() => { notificationPlaceholder.removeChild(infoBox) }, 3e3)
 }
 
 window.onload = () => { login(null) };
